7ddc2b983fe0f234db8ba56623dc87b0e2c9ced5
kabir-coderex <kabir@coderex.co>
2025-09-07 18:47:51 +0600

    fix the routes

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('api/v1', router);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
