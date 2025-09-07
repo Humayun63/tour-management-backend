0793df0aaac836213b0e0aff4f25f5f72263586a
kabir-coderex <kabir@coderex.co>
2025-09-07 23:16:54 +0600

    route not found handler added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './app/routers';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express();
 
@@ -18,5 +19,7 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.use(globalErrorHandler)
+
+app.use(notFound)
 
 export default app;
