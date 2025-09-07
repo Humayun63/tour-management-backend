c681e8ecd199bc7ac6ff03e7940b2be3177dbe9f
kabir-coderex <kabir@coderex.co>
2025-09-07 23:11:39 +0600

    added custom error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
+import AppError from "../errorHelpers/AppError";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 export const globalErrorHandler =  (error: any, req: Request, res: Response, next: NextFunction) => {
 
-    const statusCode = 500;
-    const message = `Something went wrong! ${error.message}`;
+    let statusCode = 500;
+    let message = `Something went wrong! ${error.message}`;
+
+    if(error instanceof AppError) {
+        statusCode = error.statusCode;
+        message = error.message;
+    }
 
     res.status(statusCode).json({
         success: false,
