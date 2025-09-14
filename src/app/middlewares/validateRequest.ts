dbb20abde4d184f614e9a64909667b09e940223d
kabir-coderex <kabir@coderex.co>
2025-09-14 11:45:10 +0600

    validation added with zod on create user

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,9 @@
+import {ZodObject} from "zod";
+import { catchAsync } from "../utils/catchAsync";
+import { NextFunction, Request, Response } from "express"
+
+
+export const validateRequest = (zodSchema: ZodObject) => catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await zodSchema.parseAsync(req.body);
+    next();
+}); 
