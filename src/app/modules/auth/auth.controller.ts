06a9ec7d5c4b3d1dd77ac6ef632c7bdba1800e0f
kabir-coderex <kabir@coderex.co>
2025-09-15 21:30:55 +0600

    access token route created with cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,9 +3,15 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from 'http-status-codes'
 import { AuthServices } from "./auth.services";
+import AppError from "../../errorHelpers/AppError";
 
 export const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const result = await AuthServices.credentialsLogin(req.body);
+
+    res.cookie('refreshToken', result.refreshToken, {
+        httpOnly: true,
+        secure: false,
+    })
 
     sendResponse(res, {
         statusCode: httpStatus.OK,
@@ -16,8 +22,10 @@ export const credentialsLogin = catchAsync(async (req: Request, res: Response, n
 });
 
 export const getAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const refreshToken = req.headers.authorization;
-
+    const refreshToken = req.cookies.refreshToken;
+    if(!refreshToken){
+        throw new AppError(httpStatus.BAD_REQUEST, 'No Refresh token provided')
+    }
     const result = await AuthServices.getAccessToken(refreshToken as string);
 
     sendResponse(res, {
